6a564ad890023100ebec5a4c51b433c7ecc1af90
Reid Spencer <reid@viritude.com>
2014-11-16 20:21:41 -0500

    Initial Version

---
+++ project/Build.scala
@@ -0,0 +1,22 @@
+import play.twirl.sbt.Import.TwirlKeys
+import sbt.Keys._
+import sbt._
+import play.twirl.sbt.SbtTwirl
+
+object Build extends Build {
+
+  lazy val our_settings : Seq[Setting[_]] = Seq(
+    name := "twirl-bug",
+    version := "1.0",
+    scalaVersion := "2.11.4",
+    libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.6" % "test",
+    libraryDependencies += "com.typesafe.play" %% "twirl-api" % "1.0.3",
+    sourceDirectories in TwirlKeys.compileTemplates := (unmanagedSourceDirectories in Compile).value
+  )
+
+  lazy val project = Project("twirl-bug", file("."))
+    .enablePlugins(SbtTwirl)
+    .settings(settings:_*)
+
+  override def rootProject = Some(project)
+}
---
+++ src/main/scala/com/example/Hello.scala
@@ -0,0 +1,9 @@
+package com.example
+
+import _root_.com.example.txt.hello_world
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println(hello_world(args))
+  }
+}
